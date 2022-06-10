1c0b76b52d0ded049636efa55f579367a07155cb
narvikd <84069271+narvikd@users.noreply.github.com>
2022-06-10 15:05:35 +0200

    Spring defaults
    Signed-off-by: narvikd <84069271+narvikd@users.noreply.github.com>

---
+++ src/main/java/com/example/calculadorapruebatecnica/CalculadoraPruebatecnicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculadorapruebatecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraPruebatecnicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculadoraPruebatecnicaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calculadorapruebatecnica/CalculadoraPruebatecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculadorapruebatecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraPruebatecnicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
